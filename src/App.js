31aa5610bc4cd47422f3198a5d1d327ffcb0e722
Adam-Siedlecki <acs00002@mix.wvu.edu>
2023-11-14 18:18:03 -0500

    Final lab code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
+function TodoList() {
+
+   const [task, setTask] = useState("");
+   const [taskItems, setTaskItems] = useState([]);
+
+   function addItem(event) {
+      event.preventDefault();
+
+      const newItem = {
+         key: Date.now(),
+         text: task
+      };
+
+      setTaskItems(prevItems => [...prevItems, newItem]);
+	  setTask("");
+	}
+	
+	function deleteItem(key) {
+		setTaskItems(prevItems => prevItems.filter(
+			item=> item.key !== key));
+	}
+	
+   return (
+      <div className="page">
+         <h1>Todo List</h1>
+         <form onSubmit={addItem}>
+            <input id="task" type="text" placeholder="Enter your task"
+               value={task} onChange={(e) => setTask(e.target.value)} />
+            <button id="add" type="submit">Add</button>
+			
+         </form>
+		 <TodoItems items = {taskItems} delete={deleteItem}/>
+      </div>
+   );
+}
+
+function TodoItems(props) {
+	const todoItems = props.items;
+	
+	return (
+		<ol>
+		{todoItems.map((item) =>
+			<li key={item.key}>
+				{item.text} &nbsp;
+				<button onClick={()=>props.delete(item.key)}>
+					X
+				</button>
+			</li>
+		)}
+		</ol>
+	);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return <TodoList />;
 }
 
 export default App;
